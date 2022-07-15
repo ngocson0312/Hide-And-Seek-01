31d852815b798f640053b4e58d7b1077770dd567
ngocson <ngocson031201@gmail.com>
2022-07-15 17:48:57 +0700

    init file

---
+++ Assets/Scripts/EnemyHide/PatrolAgent.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PatrolAgent : MonoBehaviour
+{
+    public static PatrolAgent instance;
+    [SerializeField] private Transform[] points;
+    [SerializeField] float RenmainingDistance = 2f;
+
+    public float waitTime = 1f;
+    float time = 0;
+    public Transform GroundCheck;
+    [SerializeField] float GroundCheckDistance = 0.4f;
+    public LayerMask groundSwimMask;
+
+    public GameObject cage; //cái lồng
+
+    bool isGrounded;
+    Animator animator;
+    bool isMove;
+
+    int destinationPoint = 0;
+    private NavMeshAgent agent;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+
+        animator = GetComponentInChildren<Animator>();
+        moveToFirstPoint();
+    }
+
+
+    private void Update()
+    {
+
+        if (!agent.pathPending && agent.remainingDistance < RenmainingDistance)
+        {
+            //animator.SetFloat("MoveAnim", 1);
+            GoToNextPoint();
+        }
+
+        CheckGroundToSwim();
+    }
+    public void moveToFirstPoint()
+    {
+        int random = Random.Range(0, points.Length);
+        agent.destination = points[random].position;
+        animator.SetFloat("MoveAnim", 1);
+    }    
+    public void GoToNextPoint()
+    {
+       
+        if(points.Length ==0)
+        {
+            Debug.Log(" bạn cần setup thêm point");
+            enabled = false;
+            return;
+        }
+
+        time += Time.deltaTime;
+        if(time >waitTime)
+        {
+            animator.SetFloat("IdleAnim", 0);
+            animator.SetFloat("MoveAnim", 1);
+            agent.destination = points[destinationPoint].position;
+            destinationPoint = (destinationPoint + 1) % points.Length;
+            isMove = true;
+            time = 0;
+        }else
+        {
+            agent.destination = transform.position;
+            animator.SetFloat("MoveAnim", 0);
+            animator.SetFloat("IdleAnim", 1);
+            isMove = false;
+        }    
+      
+    }
+
+
+    public void CheckGroundToSwim()
+    {
+        isGrounded = Physics.CheckSphere(GroundCheck.position, GroundCheckDistance, groundSwimMask);
+        if (isGrounded)
+        {
+            animator.SetBool("isGroundSwim", true);
+        }
+        else
+        {
+            animator.SetBool("isGroundSwim", false);
+
+        }
+
+        if (isGrounded && isMove == false)
+        {
+            animator.SetBool("isGroundSwimIdle", true);
+        }
+
+        if (isGrounded && isMove == true)
+        {
+            animator.SetBool("isGroundSwimIdle", false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       bool Checked = Physics.CheckSphere(GroundCheck.position, GroundCheckDistance, groundSwimMask);
+        if (other.gameObject.name== "sensor")
+        {
+            cage.SetActive(true);
+            agent.speed = 0f;
+            animator.SetFloat("MoveAnim", 0);
+            if (Checked == true )
+            {
+                animator.SetBool("isGroundSwimIdle", true);
+            }
+            else
+            {
+                animator.SetFloat("IdleAnim", 1);
+            }    
+        }
+
+        if (other.gameObject.name == "AIHide")
+        {
+            cage.SetActive(false);
+            agent.speed = 1.2f;
+            animator.SetFloat("MoveAnim", 1);
+            animator.SetFloat("IdleAnim", 0);
+
+        }
+
+        if(other.gameObject.tag =="TrapSprites")
+        {
+            Destroy(this.gameObject);
+        }    
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController instance;
+    public NavMeshAgent agent;
+    [SerializeField] private Rigidbody _rg;
+    [SerializeField] private FloatingJoystick _joystick;
+    [SerializeField] private float _moveSpeed;
+    Animator animator;
+
+    public Transform GroundCheck;
+    public LayerMask groundSwimMask;
+    [SerializeField] float GroundCheckDistance = 0.4f;
+    bool isMove;
+    bool isGrounded;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    private void Start()
+    {
+        
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        //chạm vào để hiển thị nút di chuyển
+        Touch();
+
+        CheckGroundToSwim();
+
+        //kiểm tra thử về anim mở cử lồng nhốt
+        prisonAnim();
+
+        //kiểm tra thử về anim win
+        WinAnim();
+
+        //kiểm tra thử về anim close
+        CloseAnim();
+    }
+
+    public void Touch()
+    {
+
+        if (Input.GetMouseButton(0))
+        {
+            isMove = true;
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            PlayerMove();
+            animator.SetFloat("MoveAnim", 1);
+        }
+        else
+        {
+            _rg.velocity = new Vector3(_joystick.Horizontal, _rg.velocity.y, _joystick.Vertical);
+            animator.SetFloat("MoveAnim", 0);
+            animator.SetFloat("IdleAnim", 1);
+            isMove = false;
+        }
+
+    }
+
+    public void PlayerMove()
+    {
+        // di chuyển nhân vật
+        _rg.velocity = new Vector3(_joystick.Horizontal * _moveSpeed, _rg.velocity.y, _joystick.Vertical * _moveSpeed);
+
+        //Kiểm tra xem nhân vật có di chuyển hay không?
+        if (_joystick.Horizontal != 0 || _joystick.Vertical != 0)
+        {
+            //code khả năng xoay người của nhân vật
+            transform.rotation = Quaternion.LookRotation(_rg.velocity);
+
+        }
+   
+   
+    }
+
+    public void CheckGroundToSwim()
+    {
+        isGrounded = Physics.CheckSphere(GroundCheck.position, GroundCheckDistance, groundSwimMask);
+        if(isGrounded)
+        {
+            animator.SetBool("isGroundSwim", true);
+        }
+        else
+        {
+            animator.SetBool("isGroundSwim", false);
+
+        }    
+        
+        if(isGrounded && isMove ==false)
+        {
+            animator.SetBool("isGroundSwimIdle", true);
+        }
+
+        if (isGrounded && isMove == true)
+        {
+            animator.SetBool("isGroundSwimIdle", false);
+        }
+    }  
+    
+    public void prisonAnim()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetBool("isPrison", true);
+        }
+        else
+        {
+            animator.SetBool("isPrison", false);
+        }    
+    }
+
+
+    public void WinAnim()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            animator.SetBool("isWin", true);
+        }
+
+    }
+
+    public void CloseAnim()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            animator.SetBool("isClose", true);
+        }
+
+    }
+}
